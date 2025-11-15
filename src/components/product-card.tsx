539c1d2896372843e502f540267cc2bc7a9d81b6
ajmal-m <ajua76774@gmail.com>
2025-11-15 22:23:17 +0530

    Product card updates

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -41,13 +41,13 @@ const ProductCard = memo(({ product }:{ product: ProductType}) => {
         >
             <img src={ProductImage} alt="product-image" loading="lazy"/>
             <h3 className="text-[24px] font-mont text-[#000000]">{product.name}</h3>
-            <p className="font-mont text-[#000000] text-[16px] min-[600px]:text-[20px] items-left">{product.quantityType}</p>
+            <p className="font-mont text-[#000000] text-[16px] min-[600px]:text-[17px] items-left">{product.quantityType}</p>
             <div className="w-full flex items-center justify-between">
                 <p className="font-mont text-[#000000] text-[20px] min-[600px]:text-[24px]">₹ {product.price}</p>
                 {
                     selectedQuantity > 0 ? (
                         <button
-                            className="min-w-22 px-4 py-2 bg-[#0B6434] text-white rounded-[5px] font-mont text-[16px] font-semibold"
+                            className="min-w-22 min-h-11 px-4 py-2 bg-[#0B6434] text-white rounded-[5px] font-mont text-[16px] font-semibold"
                             type="button"
                         >
                             <div className="flex items-center justify-between gap-1">
@@ -58,7 +58,7 @@ const ProductCard = memo(({ product }:{ product: ProductType}) => {
                         </button>
                     ) : (
                         <button
-                            className="min-w-22 px-4 py-2 bg-[white] text-[#0B6434] border border-[#0B6434] rounded-[5px] cursor-pointer font-mont text-[16px] font-semibold"
+                            className="min-w-22 min-h-11 px-4 py-2 bg-[white] text-[#0B6434] border border-[#0B6434] rounded-[5px] cursor-pointer font-mont text-[16px] font-semibold"
                             type="button"
                             onClick={() => updateQuantity("+")}
                         >
