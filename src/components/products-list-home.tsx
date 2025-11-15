d39e9cffdbb1f1189c0ab5a0e6b7ce4cc129e426
ajmal-m <ajua76774@gmail.com>
2025-11-15 19:31:18 +0530

    Product card updates

--- src/components/products-list-home.tsx
+++ src/components/products-list-home.tsx
@@ -5,7 +5,7 @@ const Products = memo(() => {
     return(
         <section className="px-10 mt-[30px] flex flex-col gap-[30px] max-[992px]:px-4 max-[992px]:mt-4">
             <h2  className="text-[32px] font-semibold text-[#000000] font-mont max-[400px]:text-[24px]">ALL PRODUCTS</h2>
-            <div className="grid gap-4 grid-cols-2 min-[548px]:grid-cols-3 min-[716px]:grid-cols-4 min-[912px]:grid-cols-5">
+            <div className="grid gap-4 grid-cols-1 min-[370px]:grid-cols-2 min-[548px]:grid-cols-3 min-[716px]:grid-cols-4 min-[912px]:grid-cols-5">
                 {
                     new Array(20).fill(0).map((_,i) => (
                         <ProductCard key={i}/>
