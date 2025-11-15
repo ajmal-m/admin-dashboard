5ee6a98b02d481aab50f52c83a6f4b57c84469a9
ajmal-m <ajua76774@gmail.com>
2025-11-15 11:35:45 +0530

    Home Page updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
 
+const HomePage = lazy(() => import('./pages/home'));
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
+    <Routes>
+      <Route path="/" element={
+        <Suspense fallback={<>Loading...</>}>
+          <HomePage/>
+        </Suspense>
+      }/>
+    </Routes>
     </>
   )
 }
