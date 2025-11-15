2be3b1e6c49782a404b2e540c347100477bad845
ajmal-m <ajua76774@gmail.com>
2025-11-15 17:14:44 +0530

    Category style updates

--- src/components/category-container.tsx
+++ src/components/category-container.tsx
@@ -3,7 +3,7 @@ import CategorySwiper from "./category-swiper";
 
 const CategorySection = memo(() => {
     return(
-        <section className="px-10 mt-[30px] flex flex-col gap-[30px] max-[400px]:px-4">
+        <section className="px-10 mt-[30px] flex flex-col gap-[30px] max-[400px]:px-4 max-[400px]:mt-[16px]">
             <h2 className="text-[32px] font-semibold text-[#000000] font-mont max-[400px]:text-[24px]">CATEGORY</h2>
             <CategorySwiper/>
         </section>
