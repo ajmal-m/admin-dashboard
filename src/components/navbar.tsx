f890c7595dcbe62606cd246fec8fae0548fcb38a
ajmal-m <ajua76774@gmail.com>
2025-11-15 14:28:56 +0530

    Font updates

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -36,7 +36,7 @@ const Navbar = memo(() => {
         className="h-20 bg-[#FFFFFF] dark:bg-[#FFFFFF] flex items-center justify-between px-10 border-b border-green-700"
        >
             <img src={Logo} alt="web-app-logo"/>
-            <ul className="flex items-center gap-[75px]">
+            <ul className="flex items-center gap-[75px] max-sm:hidden">
                 {
                     NavLinks.map((link , i) => (
                         <li key={i}
