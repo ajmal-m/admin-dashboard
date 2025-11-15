dce40d8046a8078267cb721d1ec8366c297e935c
ajmal-m <ajua76774@gmail.com>
2025-11-15 15:00:23 +0530

    Responsive updates

--- src/components/home-banner.tsx
+++ src/components/home-banner.tsx
@@ -7,7 +7,7 @@ const HomeBanner = memo(() => {
         <section className="w-full min-h-[408px] bg-[#0B6434] dark:bg-[#0B6434] px-10 flex items-center justify-between max-[964px]:flex-col pt-6">
             <div className="flex flex-col max-[964px]:items-center max-[964px]:text-center">
                 <h1
-                    className="font-mont text-[36px] font-bold text-white leading-[150.2%] tracking-[4%] uppercase  max-[964px]:w-full"
+                    className="font-mont text-[36px] font-bold text-white leading-[150.2%] tracking-[4%] uppercase  max-[964px]:w-full max-[400px]:text-[24px] max-[400px]:leading-[150%]"
                 >
                     Healthy and Fresh Grocery
                 </h1>
@@ -16,8 +16,8 @@ const HomeBanner = memo(() => {
                     of the finest, nutrient-rich products that 
                     cater to your health conscious lifestyle.
                 </p>
-                <button className="w-[200px] h-[60px] bg-[#FFFFFF] dark:bg-[#FFFFFF] rounded-[5px] mt-[34px] uppercase cursor-pointer" type="button">
-                    <span className="text-[#0B6434] text-[24px] font-semibold uppercase leading-[150.2%]">Learn more</span>
+                <button className="w-[200px] h-[60px] bg-[#FFFFFF] dark:bg-[#FFFFFF] rounded-[5px] mt-[34px] uppercase cursor-pointer  max-[400px]:mt-[18px] max-[400px]:w-40 max-[400px]:h-10" type="button">
+                    <span className="text-[#0B6434] text-[24px] font-semibold uppercase leading-[150.2%]  max-[400px]:text-[16px]">Learn more</span>
                 </button>
             </div>
             <div>
