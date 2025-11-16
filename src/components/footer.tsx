7cb26196c3bc79e189dd1a4cc654c6f58984df32
ajmal-m <ajua76774@gmail.com>
2025-11-16 10:57:02 +0530

    Footer updates

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,32 +1,29 @@
 import { memo } from "react";
 
+
+const Sections = [
+      { title: "ABOUT", items: ["Who we are?", "Privacy Policy"] },
+      { title: "HELP", items: ["Support", "Help Center"] },
+      { title: "CONTACT", items: ["Terms & Condition", "Return & Exchange Policy"] },
+      { title: "FOLLOW US", items: ["Facebook", "Instagram"] }
+    ];
+
 const Footer = memo(() => {
     return(
-       <footer className="min-h-[100px] bg-[#C6EAD7] dark:bg-[#C6EAD7] mt-[30px]">
-        <div className="py-[30px] px-10 flex items-center justify-center flex-wrap gap-10 max-[600px]:gap-4 max-[600px]:py-2 max-[600px]:px-4 max-[600px]:justify-start">
-            <ul className="font-mont text-[14px]">
-                <li className="text-[16px] font-medium font-mont">ABOUT</li>
-                <li>Who we are?</li>
-                <li>Privacy Policy</li>
-            </ul>
-
-             <ul className="font-mont text-[14px]">
-                <li className="text-[16px] font-medium font-mont">HELP</li>
-                <li>Support</li>
-                <li>Help Center</li>
-            </ul>
-
-             <ul className="font-mont text-[14px]">
-                <li className="text-[16px] font-medium font-mont">CONTACT</li>
-                <li>Terms & Condition </li>
-                <li>Return & Exchange Policy</li>
-            </ul>
-
-             <ul className="font-mont text-[14px]">
-                <li className="text-[16px] font-medium font-mont">FOLLOW US</li>
-                <li>Facebook?</li>
-                <li>Instagram</li>
-            </ul>
+       <footer className="min-h-[100px] bg-[#C6EAD7] dark:bg-[#C6EAD7] mt-[30px] py-[30px] px-10 max-[600px]:py-2 max-[600px]:px-4 ">
+        <div className="grid grid-cols-4 max-[859px]:grid-cols-3 max-[663px]:grid-cols-2 max-[410px]:grid-cols-1 gap-4">
+            {
+                Sections.map((section) => (
+                    <ul className="font-mont text-[14px]" key={section.title}>
+                        <li className="text-[16px] font-medium">{section.title}</li>
+                        {
+                            section.items.map((item) => (
+                                <li key={item}>{item}</li>
+                            ))
+                        }
+                    </ul>
+                ))
+            }
         </div>
        </footer>
     )
