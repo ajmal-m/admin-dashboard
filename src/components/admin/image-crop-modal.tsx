c1e14145317cc8d9745b7fabfa1016c03a96d8e0
ajmal-m <ajua76774@gmail.com>
2025-11-23 15:34:57 +0530

    IMage crop-drawer updates

--- src/components/admin/image-crop-modal.tsx
+++ src/components/admin/image-crop-modal.tsx
@@ -29,31 +29,40 @@ const CloseIcon = memo( () => {
 const ImageCanvas = memo(( { image } : { image : string }) => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [isDrawing, setIsDrawing] = useState<boolean>(false);
+    const imgRef = useRef<HTMLImageElement | null>(null)
 
     const points = useRef({ start:[0,0] , end:[0,0] });
+
+
+    const drawCanvas = useCallback((img : HTMLImageElement) => {
+        const canvas = canvasRef.current;
+        if(!canvas){
+            console.log("No Canvas found");
+            return;
+        }
+        const ctx = canvas.getContext("2d");
+        
+        if(!ctx) return;
+
+        canvas.width =  img.width + 50;
+        canvas.height = img.height + 50;
+
+        ctx.fillStyle = "#ede3e3"
+
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.drawImage(img,25,25);
+
+        if(imgRef.current) return;
+        imgRef.current = img;
+    },[])
 
     useEffect(() => {
         try {
             const img = new Image();
             img.src = image;
             img.onload = () => {
-                const canvas = canvasRef.current;
-                if(!canvas){
-                    console.log("No Canvas found");
-                    return;
-                }
-                const ctx = canvas.getContext("2d");
-                
-                if(!ctx) return;
-
-                canvas.width =  img.width + 50;
-                canvas.height = img.height + 50;
-
-                ctx.fillStyle = "#ede3e3"
-
-                ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-                ctx.drawImage(img,25,25);
+                drawCanvas(img)
             }
         } catch (error) {
             console.log(error);
@@ -88,6 +97,9 @@ const ImageCanvas = memo(( { image } : { image : string }) => {
         if(!canvas) return;
         const ctx = canvas.getContext("2d");
         if(!ctx) return;
+        clearLine();
+        if(!imgRef.current) return;
+        drawCanvas(imgRef.current);
         const { start : [ x1, y1] , end:[ x2, y2] } = points.current;
         lineDraw({ ctx , startX : x1 , startY : y1 , endX : x2 , endY : y1 });
         lineDraw({ ctx , startX : x2  , startY : y1 , endX : x2, endY : y2 });
@@ -121,7 +133,20 @@ const ImageCanvas = memo(( { image } : { image : string }) => {
         const ctx = canvas.getContext("2d");
         if(!ctx) return;
         ctx.clearRect(0,0, canvas.width, canvas.height);
-    },[])
+        if(!imgRef.current) return;
+        drawCanvas(imgRef.current);
+    },[]);
+
+
+    const onMouseMonving = useCallback((e : React.MouseEvent<HTMLCanvasElement | MouseEvent>) => {
+        if(!isDrawing) return;
+        const canvas = canvasRef.current;
+        if(!canvas) return;
+        const [ left, top ] = [ e.clientX , e.clientY];
+        const data = canvas.getBoundingClientRect();
+        points.current = { ...points.current , end: [ left-data.left , top-data.top] };
+        lineDrawing();
+    },[isDrawing])
 
 
     return(
@@ -131,8 +156,13 @@ const ImageCanvas = memo(( { image } : { image : string }) => {
                 ref={canvasRef}  
                 onMouseDown={mouseDown}
                 onMouseUp={mouseUp}
+                onMouseMove={onMouseMonving}
             >
             </canvas>
+            <div className="mt-2 flex items-center justify-center">
+                <Button type="button" className={cn("bg-red-600 hover:bg-red-700 cursor-pointer")} size={'sm'} onClick={clearLine}>Clear</Button>
+                <Button type="button" className={cn("bg-green-600 hover:bg-green-700 cursor-pointer mx-2")} size={'sm'}>Done</Button>
+            </div>
         </>
     )
 });
@@ -147,10 +177,6 @@ const ImageModal = memo(({ image , close } : { image: string; close:() => void})
             </button>
             <div className="mt-4">
                 <ImageCanvas  image={image} />
-            </div>
-            <div className="mt-2">
-                <Button type="button" className={cn("bg-red-600 hover:bg-red-700 cursor-pointer")} size={'sm'} onClick={() =>clearLine}>Clear</Button>
-                <Button type="button" className={cn("bg-green-600 hover:bg-green-700 cursor-pointer mx-2")} size={'sm'}>Done</Button>
             </div>
         </div>
     )
