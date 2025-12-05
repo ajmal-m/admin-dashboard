ccc2fe29f42d1aee8ebb1c34d5375fb9f3fc19c1
ajmal-m <ajua76774@gmail.com>
2025-12-05 22:26:19 +0530

    UI updates

--- src/components/admin/category/delete-category.tsx
+++ src/components/admin/category/delete-category.tsx
@@ -14,8 +14,8 @@ const DeleteCategoryModal = memo(( { close , id } : { close : () => void; id: st
     },[])
     return(
         <div className="w-100 min-h-10 p-4 bg-[#016630] dark:bg-[#016630] rounded flex items-center justify-center flex-col">
-            <p className="text-white font-normal text-[14px] font-mont">Are you sure you want to delete this item?</p>
-            <div className="flex items-center justify-center gap-2 self-end mt-2">
+            <p className="text-white font-normal text-[14px] font-mont self-start">Are you sure you want to delete this item?</p>
+            <div className="flex items-center justify-center gap-2 self-end mt-4">
                 <Button onClick={close} className={cn("cursor-pointer font-mont")}>Cancel</Button>
                 <Button 
                     className={cn("bg-red-800 hover:bg-red-900 cursor-pointer font-mont")}
