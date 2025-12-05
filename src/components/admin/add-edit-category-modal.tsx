9bbf0104242fab7c97f1b0a7fcf816787cde904f
ajmal-m <ajua76774@gmail.com>
2025-12-05 15:57:08 +0530

    Add edit category modal updates

--- src/components/admin/add-edit-category-modal.tsx
+++ src/components/admin/add-edit-category-modal.tsx
@@ -126,8 +126,8 @@ const AddEditCategoryModal = memo(({
               accept="image/*"
               className="bg-neutral-secondary-medium border border-default-medium text-heading 
               text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body cursor-pointer"
-              required
               onChange={updateImage}
+              required
               ref={imageRef}
             />
           </div>
