f3ccb16783f2793fc4445c4f4073275b77142297
ajmal-m <ajua76774@gmail.com>
2025-12-06 21:58:40 +0530

    Add category code refactoe

--- src/components/admin/category/add-category.tsx
+++ src/components/admin/category/add-category.tsx
@@ -1,29 +1,24 @@
-import { memo } from "react";
-import PopUp from "../../pop-up";
+import { memo, useState } from "react";
+import PopUp from "@/components/pop-up-drawer";
 import AddCategoryModal from "./add-edit-category-modal";
 
 const AddCategory = memo(() => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
     return(
         <div className="w-full flex justify-end">
-            <PopUp 
-                model={
-                    (close) => (
-                        <AddCategoryModal close={close}/>
-                    )
-                } 
-                trigger={
-                    (open) => (
-                        <button 
-                            onClick={open}
-                            className="
-                                px-2 py-1 flex items-center justify-center bg-[#0B6434] text-[white] 
-                                border border-[#0B6434] rounded-[5px] cursor-pointer font-mont text-[14px] font-medium"
-                        >
-                                Add Category
-                        </button>
-                    )
-                }
-                keyProp={'add-category'}
+            <button 
+                onClick={() => setIsOpen(true)}
+                className="
+                    px-2 py-1 flex items-center justify-center bg-[#0B6434] text-[white] 
+                    border border-[#0B6434] rounded-[5px] cursor-pointer font-mont text-[14px] font-medium"
+            >
+                    Add Category
+            </button>
+            <PopUp
+                keyProp="add-category"
+                model={(close) => <AddCategoryModal close={close}/>}
+                handleClose={() => setIsOpen(false)}
+                isOpen={isOpen}
             />
         </div>
     )
