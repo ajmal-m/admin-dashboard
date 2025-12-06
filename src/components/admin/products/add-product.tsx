10bada7f745b9d773674791fbb239802168a4ec0
ajmal-m <ajua76774@gmail.com>
2025-12-06 22:02:04 +0530

    add product code refcator

--- src/components/admin/products/add-product.tsx
+++ src/components/admin/products/add-product.tsx
@@ -1,29 +1,24 @@
-import { memo } from "react";
-import PopUp from "../../pop-up";
+import { memo, useState } from "react";
+import PopUp from "@/components/pop-up-drawer";
 import AddEditProduct from "./add-edit-product-modal";
 
 const AddProduct = memo(() => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
     return(
         <div className="w-full flex justify-end">
-            <PopUp 
-                model={
-                    (close) => (
-                        <AddEditProduct close={close} />
-                    )
-                } 
-                trigger={
-                    (open) => (
-                        <button 
-                            onClick={open}
-                            className="
-                                px-2 py-1 flex items-center justify-center bg-[#0B6434] text-[white] 
-                                border border-[#0B6434] rounded-[5px] cursor-pointer font-mont text-[14px] font-medium"
-                        >
-                                Add Product
-                        </button>
-                    )
-                }
-                keyProp={'add-category'}
+            <button 
+                onClick={() => setIsOpen(true)}
+                className="
+                    px-2 py-1 flex items-center justify-center bg-[#0B6434] text-[white] 
+                    border border-[#0B6434] rounded-[5px] cursor-pointer font-mont text-[14px] font-medium"
+            >
+                    Add Product
+            </button>
+            <PopUp
+                keyProp='add-product'
+                model={(close) => <AddEditProduct close={close}/>}
+                handleClose={() => setIsOpen(false)}
+                isOpen={isOpen}
             />
         </div>
     )
