c652c98b23d2766b4c7be1a9fbfc2c43ba4bafb4
ajmal-m <ajua76774@gmail.com>
2025-12-07 23:17:08 +0530

    Cart Items listing added

--- src/components/cart-item-list.tsx
+++ src/components/cart-item-list.tsx
@@ -1,4 +1,6 @@
+import type { RootState } from "@/redux/store";
 import React from "react";
+import { useSelector } from "react-redux";
 
 interface DrawerProps {
   isOpen: boolean;
@@ -7,6 +9,7 @@ interface DrawerProps {
 }
 
 const Drawer: React.FC<DrawerProps> = ({ isOpen, onClose, width = "w-96" }) => {
+    const { productQuantity, cartProducts} = useSelector((store : RootState) => store.cart)
   return (
     <>
       {/* Backdrop */}
@@ -24,35 +27,19 @@ const Drawer: React.FC<DrawerProps> = ({ isOpen, onClose, width = "w-96" }) => {
           transition-transform duration-300 overflow-y-auto
           ${width}
           ${isOpen ? "translate-x-0" : "translate-x-full"}
-          bg-white border border-green-800
+          bg-green-900 border border-green-800 rounded
         `}
       >
-        <div className="border-b border-default pb-4 mb-5 flex items-center">
-          <h5 className="inline-flex items-center text-lg font-medium text-body">
-            <svg
-              className="w-5 h-5 me-1.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-              />
-            </svg>
-            Right drawer
+        <div className="flex items-center justify-between">
+          <h5 className="inline-flex items-center text-[24px] font-mont text-white">
+            Cart Items
           </h5>
 
           <button
             type="button"
             onClick={onClose}
-            className="text-body bg-transparent hover:text-heading hover:bg-neutral-tertiary rounded-base w-9 h-9 absolute top-2.5 end-2.5 flex items-center justify-center"
+            className="text-body bg-transparent hover:text-heading hover:bg-neutral-tertiary 
+            rounded-base w-9 h-9 flex items-center justify-center cursor-pointer"
           >
             <svg
               className="w-5 h-5"
@@ -60,11 +47,11 @@ const Drawer: React.FC<DrawerProps> = ({ isOpen, onClose, width = "w-96" }) => {
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
-              fill="none"
+              fill="white"
               viewBox="0 0 24 24"
             >
               <path
-                stroke="currentColor"
+                stroke="white"
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
@@ -74,49 +61,42 @@ const Drawer: React.FC<DrawerProps> = ({ isOpen, onClose, width = "w-96" }) => {
           </button>
         </div>
 
-        <p className="mb-3 text-sm text-body">
-          Upgrade your Figma toolkit with a design system built on top{" "}
-          <a className="font-medium text-heading underline hover:no-underline" href="#">
-            Flowbite CSS
-          </a>
-          featuring variants, style guide and auto layout.
-        </p>
 
-        <p className="mb-5 text-sm text-body">
-          Recommended for professional developers and companies building enterprise-level.
-        </p>
-
-        <div className="flex items-center gap-4">
-          <button
-            type="button"
-            className="text-body bg-neutral-secondary-medium border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5"
-          >
-            Pricing & FAQ
-          </button>
-          <button
-            type="button"
-            className="inline-flex items-center justify-center text-white bg-brand border border-transparent hover:bg-brand-strong shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5"
-          >
-            Get access
-            <svg
-              className="rtl:rotate-180 w-4 h-4 ms-1.5 -me-0.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M19 12H5m14 0-4 4m4-4-4-4"
-              />
-            </svg>
-          </button>
+        <div className="flex flex-col gap-2 mt-4">
+            {
+                Object.keys(productQuantity).map(( productKey ) => (
+                    <div className="w-full min-h-15 bg-white rounded py-2 px-3 flex items-center justify-between" key={productKey}>
+                        <div className="flex items-center gap-4">
+                            <div>
+                            <img src={cartProducts[productKey].image.secure_url} alt={cartProducts[productKey].name} className="w-15 h-15"/>
+                            </div>
+                            <div className="flex flex-col">
+                                <p className="text-black text-[16px] font-mont capitalize">{cartProducts[productKey].name}</p>
+                                <p className="text-black text-[16px] font-mont capitalize font-bold">₹{cartProducts[productKey].price}</p>
+                            </div>
+                        </div>
+                        <div >
+                            <button
+                                className="w-20 h-8 flex items-center justify-center bg-[#0B6434] text-white 
+                                rounded font-mont text-[16px] px-4"
+                            >
+                                <div className="w-full grid grid-cols-3 gap-1">
+                                    <div className="flex justify-end">
+                                        <span className="cursor-pointer text-end" >-</span>
+                                    </div>
+                                    <span>{productQuantity[productKey]}</span>
+                                    <div className="flex justify-start">
+                                        <span className="cursor-pointer text-justify" >+</span>
+                                    </div>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
+
+      
       </div>
     </>
   );
