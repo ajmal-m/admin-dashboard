b4444ccc81f6a6a41a55cdf176f203d389f484b7
ajmal-m <ajua76774@gmail.com>
2025-12-07 12:40:33 +0530

    scroll top added

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -2,10 +2,12 @@ import { memo } from "react";
 import Navbar from "../components/navbar";
 import { Outlet } from "react-router";
 import Footer from "@/components/footer";
+import ScrollToTop from "@/utils/scrollTop";
 
 const MainLayout = memo(() => {
     return(
         <>
+            <ScrollToTop/>
             <Navbar/>
             <Outlet/>
             <Footer/>
