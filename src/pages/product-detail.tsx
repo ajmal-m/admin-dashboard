54a3f856b12fbfd05e9ba5e5c77858213965ed47
ajmal-m <ajua76774@gmail.com>
2025-12-07 13:03:24 +0530

    UI updates

--- src/pages/product-detail.tsx
+++ src/pages/product-detail.tsx
@@ -6,6 +6,7 @@ const ProductDetail = memo(() => {
     return(
         <section className="min-h-screen px-10 max-[992px]:px-4 mt-3">
             <h1>Product Detail {params.pId}</h1>
+            <img src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/da/cms-assets/cms/product/321082da-934f-48a8-b340-f42ba20136a7.jpg" alt="" srcset="" />
         </section>
     )
 });
