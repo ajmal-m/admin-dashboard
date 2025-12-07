862f74b29c70881a9a9f76d5ae3a95c1a5f62e3b
ajmal-m <ajua76774@gmail.com>
2025-12-07 11:37:35 +0530

    Navbar link removed

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,34 +1,8 @@
 import { memo } from "react";
 import Logo from '../assets/Grocery_Logo 1.svg';
-import { NavLink } from "react-router";
-import { Badge } from "./ui/badge";
 import CartContainer from "./cart-container";
 
 
-
-const NavLinks = [
-    {
-        name:"Home",
-        url:"/"
-    },
-    {
-        name:"About",
-        url:"/about"
-    },
-    {
-        name:"Service",
-        url:"/service"
-    },
-    {
-        name:"Product",
-        url:"/products"
-    },
-     {
-        name:"Contact",
-        url:"/contact"
-    },
-    
-];
 
 const Navbar = memo(() => {
     return(
@@ -36,24 +10,6 @@ const Navbar = memo(() => {
         className="h-20 bg-[#FFFFFF] dark:bg-[#FFFFFF] flex items-center justify-between px-10 border-b border-green-700 max-[992px]:px-4 sticky top-0 z-50"
        >
             <img src={Logo} alt="web-app-logo" className="max-[400px]:w-12 max-[400px]:h-12" loading="lazy"/>
-            <ul className="flex items-center gap-[75px] max-[996px]:gap-9 max-[850px]:hidden">
-                {
-                    NavLinks.map((link , i) => (
-                        <li key={i}
-                            className="text-[20px] font-medium font-mont"
-                        >
-                           <NavLink 
-                                to={link.url}  
-                                className={({ isActive }) =>
-                                    isActive ? "text-[#0B6434]" : "text-[#000000]"
-                                }
-                            >
-                                {link.name}
-                           </NavLink>
-                        </li>
-                    ))
-                }
-            </ul>
            <CartContainer/>
        </nav>
     )
