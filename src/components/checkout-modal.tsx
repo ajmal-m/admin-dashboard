587b4ef847174ff2e9fbede3c762cea65963e76d
ajmal-m <ajmalm76774@gmail.com>
2025-12-11 22:21:24 +0530

    Checkout modal updates

--- src/components/checkout-modal.tsx
+++ src/components/checkout-modal.tsx
@@ -20,6 +20,7 @@ const ShippingAddressForm = memo(() => {
     return(
         <>
         <form >
+            <h1 className="text-white font-mont font-medium text-[14px]">Shipping Address</h1>
             <div className="grid grid-cols-3 gap-x-3 gap-y-4 text-white mt-3">
                 <div className="flex flex-col gap-1">
                     <label htmlFor="name" className="text-[12px] font-mont font-normal">Name</label>
@@ -122,9 +123,9 @@ const ShippingAddressForm = memo(() => {
 const PaymentMethods = memo(() => {
     return(
         <div className="flex flex-col gap-4 self-start">
-            <p className="text-[12px] text-white font-medium font-mont">Payment Methods</p>
+            <p className="text-[14px] text-white font-medium font-mont">Payment Methods</p>
             <div className="flex items-center gap-2">
-                <input type="radio" name="" id="" />
+                <input type="radio" name="cod" id="cod" className="w-4 h-4" />
                 <label htmlFor="cod" className="text-[12px] text-white font-medium font-mont">Cash on Delivery</label>
             </div>
             <p className="text-blue-900 bg-white rounded-full text-center px-2 py-1 max-w-50 text-[12px]">More option will come</p>
@@ -156,15 +157,36 @@ const CheckOutModal = memo((
         })
     },[])
     return(
-        <div className="min-w-200 min-h-100 bg-green-800 rounded p-6">
-            <div className="w-full bg-white h-3 rounded-full overflow-hidden">
+        <div className="min-w-200 min-h-100 bg-green-800 rounded p-6 w-full h-full relative text-white">
+            <button className="text-[14px] font-mont font-medium text-white absolute top-6 right-6">
+                 <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    onClick={close}
+                >
+                <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18 17.94 6M18 18 6.06 6"
+                />
+                </svg>
+            </button>
+            <h1 className="text-center text-[14px] font-mont">{step+1} / {steps.length}</h1>
+            <div className="w-full bg-white h-3 rounded-full overflow-hidden mt-6">
                 <div 
                     className={cn("transition-all duration-450 ease", "bg-green-500 h-full rounded-full")} 
                     style={ {width:`${100*(step+1)/steps.length}%`} }
                 />
             </div>
-            <h1 className="text-white font-mont font-bold text-[16px] text-center mt-2">{step+1} of {steps.length} Steps</h1>
-            <div >
+          
+            <div className="mt-6" >
                 { step === 0 && <ShippingAddressForm/> }
                 { step === 1 && <PaymentMethods/> }
                 { step === 2 && <div>Step 3</div> }
