064f7390b069c2026ad24a17e7948195c0fc896d
ajmal-m <ajmalm76774@gmail.com>
2025-12-19 10:01:45 +0530

    Order card added

--- src/pages/orders.tsx
+++ src/pages/orders.tsx
@@ -10,6 +10,41 @@ import { Link } from "react-router";
 import { Oval } from "react-loader-spinner";
 
 
+const OrderCard = memo((
+    { order }: { order :Order }
+) => {
+    return(
+        <div className="min-w-10 min-h-50 border border-green-500 hover:border-green-700 rounded p-4 grid grid-cols-1 gap-y-2">
+            <div className="grid grid-cols-2 gap-4">
+                <p className="text-[14px] font-mont text-black">Items placed</p>
+                <span className="text-[14px] font-mont text-black">{order.items.length}</span>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+                <p className="text-[14px] font-mont text-black">Total payment</p>
+                <span className="text-[14px] font-mont text-black">₹{order.grandTotal}</span>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+                <p className="text-[14px] font-mont text-black">Payment status</p>
+                <span className="text-[14px] font-mont text-black">{order.payment.status}</span>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+                <p className="text-[14px] font-mont text-black">Order status</p>
+                <span className="text-[14px] font-mont text-black">{order.orderStatus}</span>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+                <p className="text-[14px] font-mont text-black">Order placed</p>
+                <span className="text-[14px] font-mont text-black">2 days ago</span>
+            </div>
+            <Link to={`/order/${order._id}`} className={cn("w-full")}>
+                <Button className={cn("rounded bg-green-800 font-mont cursor-pointer text-[16px] w-full")}>
+                    For More Details    
+                </Button>  
+            </Link>
+        </div>
+    )
+});
+
+
 const OrderSection = memo(() => {
     const userId = useSelector((store : RootState) => store.auth.id);
     const getOrdersMutation = useGetUserOrders({ id : userId });
@@ -36,33 +71,7 @@ const OrderSection = memo(() => {
             <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4">
                 {
                     orders.map((order, _) =>(
-                    <div className="min-w-10 min-h-50 border border-green-500 hover:border-green-700 rounded p-4 grid grid-cols-1 gap-y-2">
-                        <div className="grid grid-cols-2 gap-4">
-                            <p className="text-[14px] font-mont text-black">Items placed</p>
-                            <span className="text-[14px] font-mont text-black">{order.items.length}</span>
-                        </div>
-                        <div className="grid grid-cols-2 gap-4">
-                            <p className="text-[14px] font-mont text-black">Total payment</p>
-                            <span className="text-[14px] font-mont text-black">₹{order.grandTotal}</span>
-                        </div>
-                        <div className="grid grid-cols-2 gap-4">
-                            <p className="text-[14px] font-mont text-black">Payment status</p>
-                            <span className="text-[14px] font-mont text-black">{order.payment.status}</span>
-                        </div>
-                        <div className="grid grid-cols-2 gap-4">
-                            <p className="text-[14px] font-mont text-black">Order status</p>
-                            <span className="text-[14px] font-mont text-black">{order.orderStatus}</span>
-                        </div>
-                        <div className="grid grid-cols-2 gap-4">
-                            <p className="text-[14px] font-mont text-black">Order placed</p>
-                            <span className="text-[14px] font-mont text-black">2 days ago</span>
-                        </div>
-                        <Button className={cn("rounded bg-green-800 font-mont cursor-pointer text-[16px]")}>
-                            <Link to={`/order/${order._id}`}>
-                                For More Details    
-                            </Link>
-                        </Button>
-                    </div>
+                        <OrderCard order={order}/>
                     ))
                 }
             </div>
