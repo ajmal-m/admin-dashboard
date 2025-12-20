0a48a057aced14dda52c75150081cdc9957e52b7
ajmal-m <ajmalm76774@gmail.com>
2025-12-20 15:28:11 +0530

    Admin dashboard analytics card updates

--- src/pages/admin/admin-home.tsx
+++ src/pages/admin/admin-home.tsx
@@ -1,18 +1,71 @@
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 import {  memo } from "react";
+
+
+const SalesCard = memo(() => {
+    return(
+        <div className="bg-green-800 rounded p-4 min-h-20">
+            <h1 className="text-[16px] text-white font-mont font-medium">
+                Sales at Last Week
+            </h1>
+            <p className="text-[24px] text-white font-mont font-medium" >
+                $15,230
+            </p>
+        </div>
+    )
+});
+
+
+const DeliveredCard = memo(() => {
+    return(
+        <div className="bg-green-800 rounded p-4 min-h-20">
+            <h1 className="text-[16px] text-white font-mont font-medium">
+                Delivered Orders
+            </h1>
+            <p className="text-[24px] text-white font-mont font-medium" >
+                $15,230
+            </p>
+        </div>
+    )
+});
+
+const TotalCustomerCard = memo(() => {
+    return(
+        <div className="bg-green-800 rounded p-4 min-h-20">
+            <h1 className="text-[16px] text-white font-mont font-medium">
+                Total Customers
+            </h1>
+            <p className="text-[24px] text-white font-mont font-medium" >
+                $15,230
+            </p>
+        </div>
+    )
+});
+
+const AverageOrderValueCard = memo(() => {
+    return(
+        <div className="bg-green-800 rounded p-4 min-h-20">
+            <h1 className="text-[16px] text-white font-mont font-medium">
+                Average Order Value
+            </h1>
+            <p className="text-[24px] text-white font-mont font-medium" >
+                $15,230
+            </p>
+        </div>
+    )
+});
+
+
 
 const AdminHomePage = memo(() => {
     return(
-        <>
-            <h1>Admin Home Page</h1>
-            <Button
-                className={
-                    cn("bg-red-800 cursor-pointer hover:bg-red-900")
-                }
-                onClick={() => alert("My name is AJMAL")}
-            >Hello World</Button>
-        </>
+        <section className="px-10 mt-4 max-[992px]:px-4">
+            <div className="grid  grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4">
+                <SalesCard/>
+                <DeliveredCard/>
+                <TotalCustomerCard/>
+                <AverageOrderValueCard/>
+            </div>
+        </section>
     )
 });
 
