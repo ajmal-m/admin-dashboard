3049db16474c310980f000f47b072c109aeaa6de
ajmal-m <ajmalm76774@gmail.com>
2025-12-24 22:54:07 +0530

    theme updates

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -80,22 +80,20 @@ const LoginButton = () => {
 };
 
 const MoonIcon = memo(() => {
-    const {toggle } = useTheme();
     return(
-        <svg onClick={toggle} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" 
+        <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" 
         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
-        className="lucide lucide-moon-icon lucide-moon cursor-pointer">
+        className="lucide lucide-moon-icon lucide-moon">
             <path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"/>
         </svg>
     )
 });
 
 const SunIcon = memo(() => {
-    const {toggle } = useTheme();
     return(
-        <svg xmlns="http://www.w3.org/2000/svg" onClick={toggle} width="24" height="24" viewBox="0 0 24 24" fill="none"
+        <svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" fill="none"
          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
-         className="lucide lucide-sun-medium-icon lucide-sun-medium cursor-pointer"><circle cx="12" cy="12" r="4"/>
+         className="lucide lucide-sun-medium-icon lucide-sun-medium"><circle cx="12" cy="12" r="4"/>
          <path d="M12 3v1"/><path d="M12 20v1"/><path d="M3 12h1"/><path d="M20 12h1"/>
         <path d="m18.364 5.636-.707.707"/><path d="m6.343 17.657-.707.707"/>
         <path d="m5.636 5.636.707.707"/><path d="m17.657 17.657.707.707"/></svg>
@@ -104,15 +102,16 @@ const SunIcon = memo(() => {
 
 
 export const ThemeToggler = memo(() => {
-    const {theme  } = useTheme();
+    const {theme , toggle  } = useTheme();
     return(
         <button
             className={
                 cn(
                     "p-2 border border-transparent hover:border-green-900",
-                    "rounded"
+                    "rounded", "cursor-pointer"
                 )
             }
+            onClick={toggle}
         >
             {
                 theme === "dark" ? <SunIcon/> : <MoonIcon/>
